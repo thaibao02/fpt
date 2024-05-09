1f3547b25912302e569a9b67c858eca33e92b869
thaibao <thaibaovu0212@gmail.com>
2024-05-10 03:54:43 +0700

    add three folders abou jsx

---
+++ src/slot2/UIStruture/App.jsx
@@ -0,0 +1,23 @@
+const App = ()=>{
+    return(
+        <section>
+            <header>
+                <h1>
+                    A header
+                </h1>
+            </header>
+            <nav>
+                <a href="#">
+                    Nav item
+                </a>
+            </nav>
+            <main>
+                <p>this content</p>
+            </main>
+            <footer>
+                <small>&coppy: FPT university</small>
+            </footer>
+        </section>
+    );
+}
+export default App;
